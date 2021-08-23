9d7fd04a5e2ac0f5a03e48475f94b1f33d1179ec
SKT T1 Faker <18914104664@163.com>
2021-08-23 16:34:53 +0800

    Update step.py

--- step.py
+++ step.py
@@ -145,7 +145,7 @@ def getUser(user, passwd):
     try:
         user = os.environ["miUser"]
         passwd = os.environ["miPassword"]
-     except Exception:
+    except Exception:
         print("请设置环境变量miUser和miPassword")
  
 if __name__ ==  "__main__":
