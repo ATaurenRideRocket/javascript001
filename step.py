68abb865a4783eecce82b2805826d2f7b19a1eff
SKT T1 Faker <18914104664@163.com>
2021-08-30 11:39:30 +0800

    Update step.py

--- step.py
+++ step.py
@@ -91,6 +91,7 @@ def main(user, passwd, step):
     if step == '':
         print ("已设置为随机步数（40000-49999）")
         step = str(random.randint(90000,99999))
+        #step = str(random.randint(40000-49999))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
